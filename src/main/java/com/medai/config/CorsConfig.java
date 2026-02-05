b6c538d561f90018010e21dbcd769f89688b9503
Jerome Jiao <yuangj188@gmail.com>
2026-02-04 20:53:35 -0800

    enable cors for frontend

---
+++ src/main/java/com/medai/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.medai.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://vituslab.com",
+                                "https://www.vituslab.com"
+                        )
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
+            }
+        };
+    }
+}

cb3eecf2b1f17dec0bfa09417fb514ccc1040f71
Jerome Jiao <yuangj188@gmail.com>
2026-02-05 12:23:03 -0800

    update springbootapplication

--- src/main/java/com/medai/MedaiApplication.java
+++ src/main/java/com/medai/MedaiApplication.java
@@ -3,7 +3,7 @@ package com.medai;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.medai")
 public class MedaiApplication {
 
     public static void main(String[] args) {
